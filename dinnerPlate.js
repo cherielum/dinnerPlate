eb5990e0fc186e93e9981354bbdddba110414a41
Me <Cherie@My-MacBook-Pro-2.local>
2017-09-12 14:44:55 -0400

    dinner plate

---
+++ dinnerPlate.js
@@ -0,0 +1,91 @@
+
+
+background(135, 106, 17); // wooden table
+fill(223, 242, 241);
+ellipse(200, 200, 350, 350); // plate
+fill(183, 235, 240);
+ellipse(200, 200, 300, 300);
+
+
+fill(13, 227, 28);
+arc(150, 200, 100, 25, 1, 400);
+fill(44, 153, 52);
+ellipse(185, 200, 13, 13); //last pea
+ellipse(175, 200, 14, 14); //penultimate pea
+ellipse(159, 200, 20, 20); //fourth pea
+ellipse(150, 200, 20, 20); //third pea
+ellipse(135, 200, 19, 19); //second pea
+ellipse(120, 200, 15, 15); //first pea
+
+fill(13, 227, 28);
+arc(150, 230, 100, 25, 1, 400);
+fill(44, 153, 52);
+ellipse(185, 230, 13, 13); //last pea
+ellipse(175, 230, 14, 14); //penultimate pea
+ellipse(159, 230, 20, 20); //fourth pea
+ellipse(150, 230, 20, 20); //third pea
+ellipse(135, 230, 19, 19); //second pea
+ellipse(120, 230, 15, 15); //first pea
+
+fill(13, 227, 28);
+arc(150, 260, 100, 25, 1, 400);
+fill(44, 153, 52);
+ellipse(185, 260, 13, 13); //last pea
+ellipse(175, 260, 14, 14); //penultimate pea
+ellipse(159, 260, 20, 20); //fourth pea
+ellipse(150, 260, 20, 20); //third pea
+ellipse(135, 260, 19, 19); //second pea
+ellipse(120, 260, 15, 15); //first pea
+
+//carrots
+fill(240, 125, 17);
+ellipse(210, 120, 20, 50);
+ellipse(220, 120, 20, 50);
+ellipse(230, 120, 20, 50);
+ellipse(240, 120, 20, 50);
+ellipse(250, 120, 20, 50);
+ellipse(260, 120, 20, 50);
+ellipse(270, 120, 20, 50);
+ellipse(280, 120, 20, 50);
+
+//potatoes or rice
+fill(227, 218, 218);
+ellipse(300, 160, 13, 13);
+ellipse(300, 170, 13, 13);
+ellipse(300, 180, 13, 13);
+ellipse(300, 190, 13, 13);
+ellipse(300, 200, 13, 13);
+ellipse(290, 180, 13, 13);
+ellipse(280, 180, 13, 13);
+ellipse(270, 180, 13, 13);
+ellipse(270, 165, 13, 13);
+ellipse(310, 171, 13, 13);
+ellipse(310, 184, 13, 13);
+ellipse(310, 194, 13, 13);
+ellipse(310, 200, 13, 13);
+ellipse(310, 160, 13, 13);
+ellipse(290, 160, 13, 13);
+ellipse(280, 160, 13, 13);
+ellipse(290, 200, 13, 13);
+ellipse(280, 200, 13, 13);
+ellipse(280, 190, 13, 13);
+ellipse(280, 220, 13, 13);
+ellipse(280, 240, 13, 13);
+ellipse(292, 240, 13, 13);
+ellipse(300, 240, 13, 13);
+
+//baked potato
+fill(133, 76, 58);
+ellipse(130, 136, 83, 53);
+fill(250, 247, 247);
+ellipse(130, 136, 84, 8);
+
+fill(255, 174, 0);
+rect(190, 290, 40, 30, 100);
+rect(130, 290, 40, 30, 100);
+rect(160, 290, 40, 30, 100);
+rect(120, 300, 40, 30, 100);
+rect(150, 300, 40, 30, 100);
+rect(190, 300, 40, 30, 100);
+rect(220, 300, 40, 30, 100);
+rect(160, 310, 40, 30, 100);
